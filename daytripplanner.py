d03d624129f996260201a840149bc5eb0b3f786c
hbeckwith88 <110302011+hbeckwith88@users.noreply.github.com>
2022-08-02 14:43:15 -0700

    Add files via upload
    # List of destination, restaurants, and transportation.

---
+++ daytripplanner.py
@@ -0,0 +1,4 @@
+destinations = [ "Louisiana", "Texas", "California", "Chicago"]
+restaruants = [ "Hometown Buffet", "Al's BBQ", "Kitchen Story", "Chi-Town Pizza"]
+mode_of_transportation = [ "Plane", "Train", "Boat", "Cat"]
+
