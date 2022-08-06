05396ee140456d2027c5077134e96113deb0aee0
hbecwith88 <hb_devcodecamp@outlook.com>
2022-08-06 08:25:27 -0700

    HELP!!!!! ITS BROKEN

--- daytripplanner.py
+++ daytripplanner.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 print("Welcome to the day trip planner, where your dream vacation is made for you!")
 user_input = input("Do you want to start planning your trip? ")
 
@@ -7,15 +7,9 @@ if user_input == "yes":
 else:
     print("Okay, maybe next time.")
 
-    
 
-
-
-
-
-=======
 destinations = [ "Louisiana", "Texas", "California", "Chicago"]
 restaruants = [ "Hometown Buffet", "Al's BBQ", "Kitchen Story", "Chi-Town Pizza"]
 mode_of_transportation = [ "Plane", "Train", "Boat", "Cat"]
 
->>>>>>> d03d624129f996260201a840149bc5eb0b3f786c
+
