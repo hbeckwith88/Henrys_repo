51df9e614a1e47998dd653c39a81b89379d10717
hbecwith88 <hb_devcodecamp@outlook.com>
2022-08-06 09:06:13 -0700

    Completed the random selections

--- daytripplanner.py
+++ daytripplanner.py
@@ -7,12 +7,20 @@ if user_input == "yes":
 else:
     print("Okay, maybe next time.")
 
-
+import random
 destinations = [ "Louisiana", "Texas", "California", "Chicago"]
 restaruants = [ "Hometown Buffet", "Al's BBQ", "Kitchen Story", "Chi-Town Pizza"]
-mode_of_transportation = [ "Plane", "Train", "Boat", "Cat"]
+mode_of_transportation = [ "Plane", "Train", "Boat", "Cab"]
+entertainment = ["Theatre", "Football game", "Haunted house", "Night club"]
 
-import random
-print(random.choice(destinations))
-print(random.choice(restaruants))
-print(random.choice(mode_of_transportation))
+randomly_generated_destination = random.choice(destinations)
+print(f"We have selected for you {randomly_generated_destination}")
+
+randomly_generated_restaruants = random.choice(restaruants)
+print(f"We have selected for you {randomly_generated_restaruants}")
+
+randomly_generated_transportation = random.choice(mode_of_transportation)
+print(f"We have selected for you a {randomly_generated_transportation}")
+
+randomly_generated_entertainment = random.choice(entertainment)
+print(f"We have selected for you a {randomly_generated_entertainment}")
