2099b98710567c00d68d4a61a3cfa8feecd7b0b2
hbecwith88 <hb_devcodecamp@outlook.com>
2022-08-06 09:16:01 -0700

    Created my functions

--- daytripplanner.py
+++ daytripplanner.py
@@ -1,11 +1,13 @@
+def welcome_message():
+    print("Welcome to the day trip planner, where your dream vacation is made for you!")
+    user_input = input("Do you want to start planning your trip? ")
 
-print("Welcome to the day trip planner, where your dream vacation is made for you!")
-user_input = input("Do you want to start planning your trip? ")
+    if user_input == "yes":
+        print("Okay, let's begin!")
+    else:
+        print("Okay, maybe next time.")
 
-if user_input == "yes":
-    print("Okay, let's begin!")
-else:
-    print("Okay, maybe next time.")
+
 
 import random
 destinations = [ "Louisiana", "Texas", "California", "Chicago"]
@@ -13,14 +15,18 @@ restaruants = [ "Hometown Buffet", "Al's BBQ", "Kitchen Story", "Chi-Town Pizza"
 mode_of_transportation = [ "Plane", "Train", "Boat", "Cab"]
 entertainment = ["Theatre", "Football game", "Haunted house", "Night club"]
 
-randomly_generated_destination = random.choice(destinations)
-print(f"We have selected for you {randomly_generated_destination}")
+def randomly_generated_destination():
+    randomly_generated_destination = random.choice(destinations)
+    print(f"We have selected for you {randomly_generated_destination}")
 
-randomly_generated_restaruants = random.choice(restaruants)
-print(f"We have selected for you {randomly_generated_restaruants}")
+def randomly_generated_restaruants():
+    randomly_generated_restaruants = random.choice(restaruants)
+    print(f"We have selected for you {randomly_generated_restaruants}")
 
-randomly_generated_transportation = random.choice(mode_of_transportation)
-print(f"We have selected for you a {randomly_generated_transportation}")
+def randomly_generated_transportation():
+    randomly_generated_transportation = random.choice(mode_of_transportation)
+    print(f"We have selected for you a {randomly_generated_transportation}")
 
-randomly_generated_entertainment = random.choice(entertainment)
-print(f"We have selected for you a {randomly_generated_entertainment}")
+def randomly_generated_entertainment():
+    randomly_generated_entertainment = random.choice(entertainment)
+    print(f"We have selected for you a {randomly_generated_entertainment}")
