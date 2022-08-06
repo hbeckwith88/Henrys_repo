211813ef754435be54d6d1e826d33d21707d7fc3
hbecwith88 <hb_devcodecamp@outlook.com>
2022-08-06 08:18:19 -0700

    Welcome message with user_input

--- README.md
+++
@@ -1,1 +0,0 @@
-# Henrys_repo
---
+++ daytripplanner.py
@@ -0,0 +1,14 @@
+print("Welcome to the day trip planner, where your dream vacation is made for you!")
+user_input = input("Do you want to start planning your trip? ")
+
+if user_input == "yes":
+    print("Okay, let's begin!")
+else:
+    print("Okay, maybe next time.")
+
+    
+
+
+
+
+
